aa909c20131c3c3986472aec17f622d958c1fc18
soyeonpark2522 <ewsy25@naver.com>
2015-03-22 16:56:52 +0900

    completed

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -12,13 +12,22 @@
 
 // Add #define pre-processor below
 
- #define FIRST_PIN 10
+ #define FIRST_LED_PIN 10
+ #define REAR_LED_PIN 9
+
 void setup() {
   //Add your code of Lab Assignment #1
-
+ pinMode(FRONT_LED_PIN,OUTPUT);
+ pinMode(REAR_LED_PIN,OUTPUT);
 }
 
 void loop() {
   //Add your code of Lab Assignment #1  
+  digitalWrite(FRONT_LED_PIN,HIGH);
+  digitalWrite(REAR_LED_PIN,LOW);
+  delay(1000);
 
+  digitalWrite(FRONT_LED_PIN,LOW);
+  digitalWrite(REAR_LED_PIN,HIGH);
+  delay(1000);
 }
